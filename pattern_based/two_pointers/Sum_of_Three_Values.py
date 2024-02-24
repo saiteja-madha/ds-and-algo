697c02e34d7a33730af06cef3da64bf97d799c16
saiteja-madha <saitejamadha1997@gmail.com>
2024-02-23 22:02:31 -0700

    questions -> links

--- pattern_based/two_pointers/Sum_of_Three_Values.py
+++ pattern_based/two_pointers/Sum_of_Three_Values.py
@@ -1,11 +1,4 @@
-# Given an array of integers, nums, and an integer value, target, determine if there are any three integers in nums whose sum is equal to the target,
-# that is, nums[i] + nums[j] + nums[k] == target. Return TRUE if three such integers exist in the array. Otherwise, return FALSE.
-
-# Note: A valid triplet consists of elements with distinct indexes. This means, for the triplet nums[i], nums[j], and nums[k], i != j, i != k, and j != k.
-# Constraints:
-# 3 <= len(nums) <= 500
-# -10^3 <= nums[i] <= 10^3
-# -10^3 <= target <= 10^3
+# https://www.educative.io/courses/grokking-coding-interview-patterns-python/sum-of-three-values
 
 
 def find_sum_of_three(nums, target):
